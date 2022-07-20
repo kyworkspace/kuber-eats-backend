ef1082e3c1c5818c2855cd44132427ae47955336
kyworkspace <ky91zzang@naver.com>
2022-07-20 23:50:31 +0900

    env changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,11 +27,11 @@ const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
     TypeOrmModule.forRoot({
       //연결 옵션
       type: 'postgres',
-      host: 'localhost',
-      port: 5432, //env에서 불러오는 내용은 기본적으로 String이기 때문에 캐스팅 해야한다.
-      username: 'postgres',
-      password: '0923445', //localhost의 경우 password를 신경쓰지 않는다.
-      database: 'kuber-eats',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT, //env에서 불러오는 내용은 기본적으로 String이기 때문에 캐스팅 해야한다.
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD, //localhost의 경우 password를 신경쓰지 않는다.
+      database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod', // typeorm이 DB에 연결할때 데이터 베이스를 모듈이 현재 상태로 마이그레이션 한다는 의미 . 일단 dev일때만 마이그레이션 하도록함
       logging: false,
       entities: [Restaurants], //엔티티에서 만든것이 자동으로 DB에 꽂히도록 entity를 정의함
